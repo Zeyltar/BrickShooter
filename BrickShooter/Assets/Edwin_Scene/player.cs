a95f97eb17b06eea2bffd85998885c08611b0b10
Valbatoze <edwin.febvin@ynov.com>
2020-05-30 14:13:59 +0200

    modification déplacement

--- BrickShooter/Assets/Edwin_Scene/player.cs
+++ BrickShooter/Assets/Edwin_Scene/player.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    public GameObject Cube;
-    public float ms;
+    public float speed;
     private Vector3 go_left;
     private Vector3 go_right;
     // Start is called before the first frame update
     void Start()
     {
-        go_left = new Vector3(-ms, 0f, 0f);
-        go_right = new Vector3(ms, 0f, 0f);
+        go_left = new Vector3(-speed, 0f, 0f);
+        go_right = new Vector3(speed, 0f, 0f);
     }
 
     // Update is called once per frame
@@ -20,11 +19,11 @@ public class player : MonoBehaviour
     {
         if (Input.GetKey("q"))
         {
-            transform.Translate(go_left);
+            transform.Translate(go_left * Time.deltaTime);
         }
         if (Input.GetKey("d"))
         {
-            transform.Translate(go_right);
+            transform.Translate(go_right * Time.deltaTime);
         }
 
         if (Input.GetMouseButton(0));
