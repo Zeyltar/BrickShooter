efb2a9db7650db1645e582568221d2d995b4ae46
Valbatoze <edwin.febvin@ynov.com>
2020-06-03 13:33:45 +0200

    ajout des différentes scenes

---
+++ BrickShooter/Assets/Scripts/LoadScenes.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenes : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadSetting()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadShop()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
